0d9ff6768c41aa57f01d3adcf61c0841fa67ea63
jm4293 <dlwoals4293@gmail.com>
2024-03-26 22:48:59 +0900

    frontend - react daum post

---
+++ src/util/daum-postcode/daumPostcode.tsx
@@ -0,0 +1,36 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
+
+interface PostcodeProps {
+  setState: Dispatch<SetStateAction<any>>;
+}
+
+export const DaumPostcode = ({ setState }: PostcodeProps) => {
+  const open = useDaumPostcodePopup('https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js');
+
+  const handleComplete = (data: any) => {
+    setState({
+      zipCode: data.zonecode,
+      address: data.roadAddress,
+    });
+  };
+
+  const handleClose = (data: any) => {
+    if (data === 'FORCE_CLOSE') {
+      setState({
+        zipCode: '',
+        address: '',
+      });
+    }
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete, onClose: handleClose });
+  };
+
+  return (
+    <button className="btn-confirm-no-background-color" type="button" onClick={handleClick}>
+      찾기
+    </button>
+  );
+};
